e4623b1a17cf9709c4995e8f949f2b158b0bc7d2
Julio Alcantara <jalcantara88@gmail.com>
2020-12-01 13:30:44 -0800

    everything except gallery is ready

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,12 @@ $(function() {
     $(".imgThumb").click(function() {
         $("#imgModal").modal("show");
     });
+
+    $("#contactButton").click(function() {
+        $("#contactModal").modal("show");
+    });
+
+    $("#closeModal").click(function() {
+        $("#imgModal").modal("close");
+    });
 });
